7d6728af95c772914e71a1a121c2bcfe77675414
Nikolay <kolekdoma@mail.ru>
2021-07-03 22:19:20 +0300

    Добавил меню

--- LoadAndViewPicturesWindowsFormsApp/LoadAndViewPicturesWindowsFormsApp/Program.cs
+++ LoadAndViewPicturesWindowsFormsApp/LoadAndViewPicturesWindowsFormsApp/Program.cs
@@ -16,7 +16,7 @@ namespace LoadAndViewPicturesWindowsFormsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
